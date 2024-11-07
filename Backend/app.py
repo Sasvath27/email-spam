14a80c79d5c92989ba20138782c85240c3065111
Sasvath27 <sasvathkr@gmail.com>
2024-11-07 10:03:10 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import pandas as pd
+import os
+import string
+import nltk
+from nltk.corpus import stopwords
 
 # Initialize the Flask app and enable CORS
 app = Flask(__name__)
@@ -14,13 +18,10 @@ with open('spam_detector_model.pkl', 'rb') as model_file:
 with open('vectorizer.pkl', 'rb') as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
 
-# Text cleaning function (use the same as during training)
-import string
-import nltk
-from nltk.corpus import stopwords
-
+# Download NLTK stopwords if not already available
 nltk.download('stopwords')
 
+# Text cleaning function
 def clean_text(text):
     text = text.lower()
     text = ''.join([char for char in text if char not in string.punctuation])
@@ -31,21 +32,18 @@ def clean_text(text):
 # Define the prediction endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input data from the request
     data = request.json
     message = data.get('message', '')
 
-    # Clean and transform the input message
     cleaned_message = clean_text(message)
     transformed_message = vectorizer.transform([cleaned_message])
 
-    # Make prediction
     prediction = model.predict(transformed_message)
     category = 'spam' if prediction[0] == 1 else 'ham'
 
-    # Return the result as a JSON response
     return jsonify({'message': message, 'prediction': category})
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
